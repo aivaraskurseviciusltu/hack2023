7b1ff45cb5e3cecf40f58740784232f5cba686c5
Matas Dregva <Matas.Dregva@ibm.com>
2023-11-25 16:41:35 +0200

    added report form

--- src/scenes/report/index.jsx
+++ src/scenes/report/index.jsx
@@ -1,91 +1,129 @@
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControl
+} from "@mui/material";
 import { Formik } from "formik";
+import { styled } from "@mui/material/styles";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import Header from "../../components/Header";
+import { useState } from "react"
 
+import React from "react"
 const Report = () => {
-  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const [age, setAge] = useState('');
 
   const handleFormSubmit = (values) => {
     console.log(values);
   };
+  const handleCategory = (event) => {
+    setAge(event.target.value);
+  };
+
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  });
 
   return (
-    <Box m="20px">
+    <Box display="grid"  gap="30px" m="20px">
       <Header title="Add Report" subtitle="Create a New Report" />
-
       <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
-            >
-              
-              <TextField
-                fullWidth
-                multiline
-                variant="filled"
-                type="text"
-                label="Description"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.address2}
-                name="address2"
-                error={!!touched.address2 && !!errors.address2}
-                helperText={touched.address2 && errors.address2}
-                sx={{ gridColumn: "span 4" }}
-              />
-            </Box>
-            <Box display="flex" justifyContent="end" mt="20px">
-              <Button type="submit" color="secondary" variant="contained">
-                Create Report
-              </Button>
-            </Box>
-          </form>
+  onSubmit={handleFormSubmit}
+  initialValues={initialValues}
+  validationSchema={checkoutSchema}
+>
+  {({
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  }) => (
+    <Box display="grid" fullWidth gap="30px">
+        <FormControl  variant="filled" fullWidth>
+          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleCategory}
+          >
+            <MenuItem value={"War"}>War</MenuItem>
+            <MenuItem value={"Shelter"}>Shelter</MenuItem>
+            <MenuItem value={"Help"}>Help</MenuItem>
+            <MenuItem value={"Medical Help"}>Medical Help</MenuItem>
+          </Select>
+        </FormControl>
+       <TextField
+          fullWidth
+          multiline
+          variant="filled"
+          type="text"
+          label="Description"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.description}
+          name="description"
+          error={!!touched.description && !!errors.description}
+          helperText={touched.description && errors.description}
+        />  
+         <TextField
+        fullWidth
+        multiline
+        variant="filled"
+        type="text"
+        label="Location"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.lastName}
+        name="lastName"
+        error={!!touched.lastName && !!errors.lastName}
+        helperText={touched.lastName && errors.lastName}
+      />
+      <Button
+          component="label"
+          variant="outlined"
+          color="secondary"
+          startIcon={<CloudUploadIcon />}
+        >
+          Upload file
+          <VisuallyHiddenInput type="file" />
+        </Button>
+        <Button type="submit" color="secondary" variant="contained">
+          Create
+        </Button>
+        </Box>
         )}
-      </Formik>
+        </Formik>
     </Box>
   );
 };
 
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
 const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, "Phone number is not valid")
-    .required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
+  description: yup.string().required("required"),
+  location: yup.string().required("required"),
 });
+
 const initialValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  contact: "",
-  address1: "",
-  address2: "",
+  description: "",
+  location: "", 
 };
 
 export default Report;
