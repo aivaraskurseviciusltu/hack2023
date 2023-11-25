6f08b6b76f0b80384b5f871987ba575d456752e2
aivaraskurseviciusltu <aivaras.kursevicius@ibm.com>
2023-11-25 20:34:23 +0200

    pins

--- src/contexts/Map.context.js
+++ src/contexts/Map.context.js
@@ -7,59 +7,59 @@ const MapContext = createContext({});
 const MapContextProvider = ({ children }) => {
   const [markers, setMarkers] = useState([
     {
-      description: "Your Current Location",
+      description: "",
       latitude: 54.6943,
       longitude: 25.2836,
       iconType: "currentLocation",
     },
     {
-      description: "Enemy",
+      description: "We have available MREs here",
       image:
         "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
       latitude: 54.69489908237602,
       longitude: 25.28025837421105,
-      iconType: "Enemy",
+      iconType: "Resources",
     },
     {
-      description: "Explosion",
+      description: "Have available 1 room with 2 beds, for any victim of invasion",
       image:
         "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
       latitude: 54.693490273002475,
       longitude: 25.281228157788654,
-      iconType: "Explosion",
+      iconType: "ShelterNoCount",
     },
     {
-      description: "Help",
+      description: "Bridge bombing recently",
       image:
         "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
       latitude: 54.69100172468465,
       longitude: 25.279849937821062,
-      iconType: "Help",
+      iconType: "Explosion",
     },
     {
-      description: "Pickup",
+      description: "We offer first aid for any survivors or recent bombing",
       image:
         "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
       latitude: 54.693700503812785,
       longitude: 25.275498460675088,
-      iconType: "Pickup",
+      iconType: "Help",
     },
     {
-      description: "Resources",
+      description: "Enemy troops marching through",
       image:
         "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
       latitude: 54.69970960219711,
       longitude: 25.2833959662436,
-      iconType: "Resources",
+      iconType: "Enemy",
     },
 
     {
-      description: "ShelterNoCount",
+      description: "I've been hurt, can't walk, SOS",
       image:
         "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
       latitude: 54.69955928345105,
       longitude: 25.286470374417043,
-      iconType: "ShelterNoCount",
+      iconType: "Pickup",
     },
   ]);
 
