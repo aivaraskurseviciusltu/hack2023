7e37abec1dc2e77d79f7e45e026965e2f6cb2975
Hackathonltu <testaswebsite@gmail.com>
2023-11-25 18:58:19 +0200

    Map

--- src/components/Map/pin.jsx
+++ src/components/Map/pin.jsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-
-const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
-  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
-  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+import { ReactComponent as Enemy } from "./icons/Enemy.svg";
+import { ReactComponent as Explosion } from "./icons/Explosion.svg";
+import { ReactComponent as Help } from "./icons/Help.svg";
+import { ReactComponent as Pickup } from "./icons/Pickup.svg";
+import { ReactComponent as Resources } from "./icons/Resources.svg";
+import { ReactComponent as ShelterNoCount } from "./icons/ShelterNoCount.svg";
 
 const pinStyle = {
   cursor: "pointer",
@@ -10,12 +12,35 @@ const pinStyle = {
   stroke: "none",
 };
 
-function Pin({ size = 20 }) {
-  return (
-    <svg height={size} viewBox="0 0 24 24" style={pinStyle}>
-      <path d={ICON} />
-    </svg>
-  );
+const CURRENT_LOCATION = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+
+function Pin({ size = 60, iconType }) {
+  const getIcon = () => {
+    switch (iconType) {
+      case "Enemy":
+        return <Enemy height={size} />;
+      case "Explosion":
+        return <Explosion height={size} />;
+      case "Help":
+        return <Help height={size} />;
+      case "Pickup":
+        return <Pickup height={size} />;
+      case "Resources":
+        return <Resources height={size} />;
+      case "ShelterNoCount":
+        return <ShelterNoCount height={size} />;
+      default:
+        return (
+          <svg height={20} viewBox="0 0 24 24" style={pinStyle}>
+            <path d={CURRENT_LOCATION} />
+          </svg>
+        );
+    }
+  };
+  console.log(getIcon());
+  return getIcon();
 }
 
 export default React.memo(Pin);
--- src/contexts/Map.context.js
+++ src/contexts/Map.context.js
@@ -7,11 +7,59 @@ const MapContext = createContext({});
 const MapContextProvider = ({ children }) => {
   const [markers, setMarkers] = useState([
     {
+      description: "Your Current Location",
       latitude: 54.6943,
       longitude: 25.2836,
-      description: "Test",
-      category: "myLocation",
-      img: "./test.png",
+      iconType: "currentLocation",
+    },
+    {
+      description: "Enemy",
+      image:
+        "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
+      latitude: 54.69489908237602,
+      longitude: 25.28025837421105,
+      iconType: "Enemy",
+    },
+    {
+      description: "Explosion",
+      image:
+        "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
+      latitude: 54.693490273002475,
+      longitude: 25.281228157788654,
+      iconType: "Explosion",
+    },
+    {
+      description: "Help",
+      image:
+        "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
+      latitude: 54.69100172468465,
+      longitude: 25.279849937821062,
+      iconType: "Help",
+    },
+    {
+      description: "Pickup",
+      image:
+        "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
+      latitude: 54.693700503812785,
+      longitude: 25.275498460675088,
+      iconType: "Pickup",
+    },
+    {
+      description: "Resources",
+      image:
+        "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
+      latitude: 54.69970960219711,
+      longitude: 25.2833959662436,
+      iconType: "Resources",
+    },
+
+    {
+      description: "ShelterNoCount",
+      image:
+        "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
+      latitude: 54.69955928345105,
+      longitude: 25.286470374417043,
+      iconType: "ShelterNoCount",
     },
   ]);
 
