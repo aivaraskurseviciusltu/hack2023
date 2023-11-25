1f649c64a4367281b09bd3be0d22ae955333cc6f
aivaraskurseviciusltu <aivaras.kursevicius@ibm.com>
2023-11-25 12:56:44 +0200

    2nd commit

--- js/store.js
+++
@@ -1,9 +0,0 @@
-var redux = require('redux');
-var createStore = redux.createStore;
-var applyMiddleware = redux.applyMiddleware;
-var thunk = require('redux-thunk').default;
-
-var reducers = require('./reducers/index');
-
-var store = createStore(reducers.escapeReducer, applyMiddleware(thunk));
-module.exports = store;
